e52b000a456ff7f69bfbf3b485b9581277a4122c
Leonardo Orellana <lorellana672@lab78.famaf.unc.edu.ar>
2019-06-11 15:06:37 -0300

    Now the algorithm is working as it should

--- intenumcomp.jl
+++ intenumcomp.jl
@@ -1,25 +1,27 @@
 function intenumcomp(fun,a,b,N,regla)
 
 x=range(a,stop=b,length=N)
-w=0
-h=(b-a)/N
+  w=0
+  h=(b-a)/N
 
-if (regla == "trapecio")
-  for i=1:(N-1)
-    fa=eval(fun(x(i)))
-    fb=eval(fun(x(i+1)))
-    w=w+ ((fa+fb)/2)*(h)
+  if (regla == "trapecio")
+    for i=1:(N-1)
+      fa=eval(fun(x[i]))
+      fb=eval(fun(x[i+1]))
+      w=w+ ((fa+fb)/2)*(h)
+    end
+  elseif (regla == "simpson")
+    for i=1:(N-1)
+      fa=eval(fun( x[i] ))
+      fb=eval(fun(x[i+1]))
+      fab=eval(fun((x[i+1]+x[i])/2))
+      w=w+ ((h)/6)*(fa+4*fab+fb)
+    end
+  else
+    for i=1:(N-1)
+      fab=eval(fun((x[i+1]+x[i])/2))
+      w=w+ fab*(h)
+    end
   end
-else if (regla == "simpson")
-  for i=1:(N-1)
-    fa=eval(fun( x(i) ))
-    fb=eval(fun(x(i+1)))
-    fab=eval(fun((x(i+1)+x(i))/2))
-    w=w+ ((h)/6)*(fa+4*fab+fb)
-  end
-else
-  for i=1:(N-1)
-    fab=eval(fun((x(i+1)+x(i))/2))
-    w=w+ fab*(h)
-  end
+  return w
 end
